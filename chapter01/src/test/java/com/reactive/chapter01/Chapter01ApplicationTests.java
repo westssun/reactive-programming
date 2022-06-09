59e9ebd8c2b14b76d7865448a8056857a9f3daa1
seohaekim <seohae0406@gmail.com>
2022-06-09 20:08:34 +0900

    (06.09) project init

---
+++ chapter01/src/test/java/com/reactive/chapter01/Chapter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.chapter01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
